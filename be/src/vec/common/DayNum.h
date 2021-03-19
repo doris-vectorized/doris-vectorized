0c894473ab5248462a4420c463a7d704c15cd844
lihaopeng <lihaopeng@baidu.com>
2021-03-19 19:25:07 +0800

    [doris-1063][vectorized] support clickhouse vec code in doris
    Change-Id: Ifbd3f928c47ac04eea362485f0e2d0f28292b9d9

---
+++ be/src/vec/common/DayNum.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vec/common/Types.h>
+#include <vec/common/strong_typedef.h>
+
+/** Represents number of days since 1970-01-01.
+  * See DateLUTImpl for usage examples.
+  */
+STRONG_TYPEDEF(UInt16, DayNum)
