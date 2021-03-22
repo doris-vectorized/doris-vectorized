092fc24e90486c2877c863bf22dc5267280dc43b
lihaopeng <lihaopeng@baidu.com>
2021-03-22 16:22:47 +0800

    [doris-1063][vectorized] support row batch convert to vec block
    Change-Id: I74c0d75096f59c7c35ab7c16f3556372e1a13f9a

--- build.sh
+++ build.sh
@@ -35,6 +35,14 @@ export DORIS_HOME=${ROOT}
 
 . ${DORIS_HOME}/env.sh
 
+# build thirdparty libraries if necessary
+if [[ ! -f ${DORIS_THIRDPARTY}/installed/lib/libs2.a ]]; then
+    echo "Thirdparty libraries need to be build ..."
+    ${DORIS_THIRDPARTY}/build-thirdparty.sh
+fi
+
+PARALLEL=$[$(nproc)+1]
+
 # Check args
 usage() {
   echo "
