ddb7bdd6f616bab77e8e9bd7d91e090f8f25c25e
lihaopeng <lihaopeng@baidu.com>
2021-03-22 16:22:47 +0800

    [doris-1063][vectorized] support row batch convert to vec block
    Change-Id: I74c0d75096f59c7c35ab7c16f3556372e1a13f9a

--- be/src/runtime/descriptors.cpp
+++ be/src/runtime/descriptors.cpp
@@ -27,6 +27,8 @@
 #include "gen_cpp/PlanNodes_types.h"
 #include "gen_cpp/descriptors.pb.h"
 
+#include "vec/Core/ColumnsWithTypeAndName.h"
+
 namespace doris {
 using boost::algorithm::join;
 
@@ -80,6 +82,14 @@ void SlotDescriptor::to_protobuf(PSlotDescriptor* pslot) const {
     pslot->set_col_name(_col_name);
     pslot->set_slot_idx(_slot_idx);
     pslot->set_is_materialized(_is_materialized);
+}
+
+DB::MutableColumnPtr SlotDescriptor::get_empty_mutable_column() const {
+    return type().get_data_type_ptr()->createColumn();
+}
+
+DB::DataTypePtr SlotDescriptor::get_data_type_ptr() const {
+    return type().get_data_type_ptr();
 }
 
 std::string SlotDescriptor::debug_string() const {
