0196ff6c9d802beeaf945f822c95ed2e2b31c09f
lihaopeng <lihaopeng@baidu.com>
2021-03-30 13:40:43 +0800

    [doris-1063][vectorized]  1.Support nullable column string  2.Support nullable decimal column  3.Change likely to LIKELY to prevent conflict in boost
    Change-Id: I929bfbdd678ca5302b4aa13c1fcf9799ec8ad301

--- be/test/vec/Core/block_test.cpp
+++ be/test/vec/Core/block_test.cpp
@@ -32,8 +32,11 @@ TEST(BlockTest, RowBatchCovertToBlock) {
     SchemaScanner::ColumnDesc column_descs[] = {
                 {"k1", TYPE_SMALLINT, sizeof(int16_t), true},
                 {"k2", TYPE_INT,      sizeof(int32_t), false},
-                {"k3", TYPE_DOUBLE,    sizeof(double),   false}};
-    SchemaScanner schema_scanner(column_descs, 3);
+                {"k3", TYPE_DOUBLE,    sizeof(double),   false},
+                {"k4", TYPE_VARCHAR,    sizeof(StringValue),   false},
+                {"k5", TYPE_DECIMALV2,    sizeof(DecimalV2Value),   false}};
+
+    SchemaScanner schema_scanner(column_descs, sizeof(column_descs) / sizeof(SchemaScanner::ColumnDesc));
     ObjectPool object_pool;
     SchemaScannerParam param;
     schema_scanner.init(&param, &object_pool);
@@ -63,6 +66,18 @@ TEST(BlockTest, RowBatchCovertToBlock) {
         slot_desc = tuple_desc->slots()[2];
         memcpy(tuple->get_slot(slot_desc->tuple_offset()), &k3, column_descs[2].size);
 
+        // string slot
+        slot_desc = tuple_desc->slots()[3];
+        auto num_str = std::to_string(k1);
+        auto string_slot = tuple->get_string_slot(slot_desc->tuple_offset());
+        string_slot->ptr = (char*)row_batch.tuple_data_pool()->allocate(num_str.size());
+        string_slot->len = num_str.size();
+        memcpy(string_slot->ptr, num_str.c_str(), num_str.size());
+
+        slot_desc = tuple_desc->slots()[4];
+        DecimalV2Value decimalv2_num(std::to_string(k3));
+        memcpy(tuple->get_slot(slot_desc->tuple_offset()), &decimalv2_num ,column_descs[4].size);
+
         tuple_row->set_tuple(0, tuple);
         row_batch.commit_last_row();
     }
@@ -75,6 +90,8 @@ TEST(BlockTest, RowBatchCovertToBlock) {
         DB::ColumnPtr column1 = block.getColumns()[0];
         DB::ColumnPtr column2 = block.getColumns()[1];
         DB::ColumnPtr column3 = block.getColumns()[2];
+        DB::ColumnPtr column4 = block.getColumns()[3];
+        DB::ColumnPtr column5 = block.getColumns()[4];
 
         if (i % 5 != 0) {
             ASSERT_EQ((int16_t)column1->get64(i), k1);
@@ -83,6 +100,10 @@ TEST(BlockTest, RowBatchCovertToBlock) {
         }
         ASSERT_EQ(column2->getInt(i), k2++);
         ASSERT_EQ(column3->getFloat64(i), k3);
+        ASSERT_STREQ(column4->getDataAt(i).data, std::to_string(k1).c_str());
+        auto decimal_field = column5->operator[](i).get<DB::DecimalField<DB::Decimal128>>();
+        DecimalV2Value decimalv2_num(std::to_string(k3));
+        ASSERT_EQ(DecimalV2Value(decimal_field.getValue()), decimalv2_num);
         k1++;
         k3+=0.1;
     }
