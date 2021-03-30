84fb28f4c700d8f10a97528757e5b67ec9fd62a3
fenghaoan <fenghaoan@baidu.com>
2021-03-30 18:31:27 +0800

    [doris-1063][vectorized] support vector expr
    Change-Id: If3a7478f804ceaa91de9a3e2e2ad2131711573ca

--- be/src/vec/DataTypes/NumberTraits.h
+++ be/src/vec/DataTypes/NumberTraits.h
@@ -39,10 +39,10 @@ struct Construct
     using Type = Error;
 };
 
-template <> struct Construct<false, false, 1> { using Type = UInt8; };
-template <> struct Construct<false, false, 2> { using Type = UInt16; };
-template <> struct Construct<false, false, 4> { using Type = UInt32; };
-template <> struct Construct<false, false, 8> { using Type = UInt64; };
+template <> struct Construct<false, false, 1> { using Type = Int16; };
+template <> struct Construct<false, false, 2> { using Type = Int32; };
+template <> struct Construct<false, false, 4> { using Type = Int64; };
+template <> struct Construct<false, false, 8> { using Type = Int64; };
 template <> struct Construct<false, true, 1> { using Type = Float32; };
 template <> struct Construct<false, true, 2> { using Type = Float32; };
 template <> struct Construct<false, true, 4> { using Type = Float32; };
---
+++ be/src/vec/exprs/vexpr.cpp
@@ -0,0 +1,114 @@
+#include "vec/exprs/vexpr.h"
+
+#include <fmt/format.h>
+
+#include "gen_cpp/Exprs_types.h"
+#include "vec/exprs/vectorized_fn_call.h"
+#include "vec/exprs/vslot_ref.h"
+
+namespace DB {
+using doris::Status;
+using doris::RuntimeState;
+using doris::RowDescriptor;
+using doris::TypeDescriptor;
+
+VExpr::VExpr(const doris::TExprNode& node)
+        : _node_type(node.node_type),
+          _type(TypeDescriptor::from_thrift(node.type)),
+          _data_type(_type.get_data_type_ptr()) {
+    if (node.__isset.fn) {
+        _fn = node.fn;
+    }
+}
+
+Status VExpr::prepare(RuntimeState* state, const RowDescriptor& row_desc, VExprContext* context) {
+    for (int i = 0; i < _children.size(); ++i) {
+        RETURN_IF_ERROR(_children[i]->prepare(state, row_desc, context));
+    }
+    return Status::OK();
+}
+
+Status VExpr::open(RuntimeState* state, VExprContext* context) {
+    for (int i = 0; i < _children.size(); ++i) {
+        RETURN_IF_ERROR(_children[i]->open(state, context));
+    }
+    return Status::OK();
+}
+
+void VExpr::close(doris::RuntimeState* state, VExprContext* context) {
+    for (int i = 0; i < _children.size(); ++i) {
+        _children[i]->close(state, context);
+    }
+}
+
+Status VExpr::create_expr(doris::ObjectPool* pool, const doris::TExprNode& texpr_node,
+                          VExpr** expr) {
+    switch (texpr_node.node_type) {
+    case doris::TExprNodeType::SLOT_REF: {
+        *expr = pool->add(new VSlotRef(texpr_node));
+        break;
+    }
+    case doris::TExprNodeType::FUNCTION_CALL: {
+        *expr = pool->add(new VectorizedFnCall(texpr_node));
+        break;
+    }
+    default:
+        return Status::InternalError(
+                fmt::format("Unknown expr node type: {}", texpr_node.node_type));
+    }
+    return Status::OK();
+}
+
+Status VExpr::create_tree_from_thrift(doris::ObjectPool* pool,
+                                      const std::vector<doris::TExprNode>& nodes, VExpr* parent,
+                                      int* node_idx, VExpr** root_expr, VExprContext** ctx) {
+    // propagate error case
+    if (*node_idx >= nodes.size()) {
+        return Status::InternalError("Failed to reconstruct expression tree from thrift.");
+    }
+    int num_children = nodes[*node_idx].num_children;
+    VExpr* expr = nullptr;
+    RETURN_IF_ERROR(create_expr(pool, nodes[*node_idx], &expr));
+    DCHECK(expr != nullptr);
+    if (parent != nullptr) {
+        parent->add_child(expr);
+    } else {
+        DCHECK(root_expr != nullptr);
+        DCHECK(ctx != nullptr);
+        *root_expr = expr;
+        *ctx = pool->add(new VExprContext(expr));
+    }
+    for (int i = 0; i < num_children; i++) {
+        *node_idx += 1;
+        RETURN_IF_ERROR(create_tree_from_thrift(pool, nodes, expr, node_idx, nullptr, nullptr));
+        // we are expecting a child, but have used all nodes
+        // this means we have been given a bad tree and must fail
+        if (*node_idx >= nodes.size()) {
+            return Status::InternalError("Failed to reconstruct expression tree from thrift.");
+        }
+    }
+    return Status::OK();
+}
+
+Status VExpr::create_expr_tree(doris::ObjectPool* pool, const doris::TExpr& texpr,
+                               VExprContext** ctx) {
+    if (texpr.nodes.size() == 0) {
+        *ctx = nullptr;
+        return Status::OK();
+    }
+    int node_idx = 0;
+    VExpr* e = nullptr;
+    Status status = create_tree_from_thrift(pool, texpr.nodes, NULL, &node_idx, &e, ctx);
+    if (status.ok() && node_idx + 1 != texpr.nodes.size()) {
+        status = Status::InternalError(
+                "Expression tree only partially reconstructed. Not all thrift nodes were used.");
+    }
+    if (!status.ok()) {
+        LOG(ERROR) << "Could not construct expr tree.\n"
+                   << status.get_error_msg() << "\n"
+                   << apache::thrift::ThriftDebugString(texpr);
+    }
+    return Status::OK();
+}
+
+} // namespace DB
