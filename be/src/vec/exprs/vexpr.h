f438e1c31c60467ec9a6276471c4cea83e1624bd
fenghaoan <fenghaoan@baidu.com>
2021-03-31 15:10:06 +0800

    [doris-1063] add some test case in vexpr
    Change-Id: I787ef30149f9aa6467ba4c5ebf40e23b2370b480

--- be/src/vec/exprs/vexpr.h
+++ be/src/vec/exprs/vexpr.h
@@ -5,8 +5,8 @@
 #include "common/status.h"
 #include "gen_cpp/Exprs_types.h"
 #include "runtime/types.h"
-#include "vec/exprs/vexpr_context.h"
 #include "vec/data_types/data_type.h"
+#include "vec/exprs/vexpr_context.h"
 
 namespace DB {
 
@@ -21,14 +21,21 @@ public:
     virtual doris::Status open(doris::RuntimeState* state, VExprContext* context);
     virtual doris::Status execute(DB::Block* block, int* result_column_id) = 0;
 
-    void add_child(VExpr*expr) {_children.push_back(expr);}
+    bool is_nullable() { return _data_type->isNullable(); }
 
-    static doris::Status create_expr_tree(doris::ObjectPool* pool, const doris::TExpr& texpr, VExprContext** ctx);
-    
-    static doris::Status create_expr(doris::ObjectPool* pool, const doris::TExprNode& texpr_node, VExpr** expr);
-    static doris::Status create_tree_from_thrift(doris::ObjectPool* pool, const std::vector<doris::TExprNode>& nodes,
-                                       VExpr* parent, int* node_idx, VExpr** root_expr,
-                                       VExprContext** ctx);
+    void add_child(VExpr* expr) { _children.push_back(expr); }
+
+    static doris::Status create_expr_tree(doris::ObjectPool* pool, const doris::TExpr& texpr,
+                                          VExprContext** ctx);
+
+    static doris::Status create_expr(doris::ObjectPool* pool, const doris::TExprNode& texpr_node,
+                                     VExpr** expr);
+
+    static doris::Status create_tree_from_thrift(doris::ObjectPool* pool,
+                                                 const std::vector<doris::TExprNode>& nodes,
+                                                 VExpr* parent, int* node_idx, VExpr** root_expr,
+                                                 VExprContext** ctx);
+
 protected:
     doris::TExprNodeType::type _node_type;
     doris::TypeDescriptor _type;
