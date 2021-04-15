fb32e610154390cbd6a1f1676d35eac6069a20d1
lihaopeng <lihaopeng@baidu.com>
2021-04-15 17:33:16 +0800

    1. Support +=*/int_div operations 2. Support limit operation in volap_scan_node
    Change-Id: Ia2e7a23dc8b6cb4d643b9add05a866921c57e9f8

---
+++ be/src/vec/functions/plus.cpp
@@ -0,0 +1,48 @@
+#include "vec/functions/simple_function_factory.h"
+#include "vec/functions/function_binary_arithmetic.h"
+#include "vec/common/arithmetic_overflow.h"
+
+namespace doris::vectorized
+{
+
+template <typename A, typename B>
+struct PlusImpl
+{
+    using ResultType = typename NumberTraits::ResultOfAdditionMultiplication<A, B>::Type;
+    static const constexpr bool allow_decimal = true;
+
+    template <typename Result = ResultType>
+    static inline NO_SANITIZE_UNDEFINED Result apply(A a, B b)
+    {
+        /// Next everywhere, static_cast - so that there is no wrong result in expressions of the form Int64 c = UInt32(a) * Int32(-1).
+        return static_cast<Result>(a) + b;
+    }
+
+    /// Apply operation and check overflow. It's used for Deciamal operations. @returns true if overflowed, false otherwise.
+    template <typename Result = ResultType>
+    static inline bool apply(A a, B b, Result & c)
+    {
+        return common::addOverflow(static_cast<Result>(a), b, c);
+    }
+
+#if USE_EMBEDDED_COMPILER
+    static constexpr bool compilable = true;
+
+    static inline llvm::Value * compile(llvm::IRBuilder<> & b, llvm::Value * left, llvm::Value * right, bool)
+    {
+        return left->getType()->isIntegerTy() ? b.CreateAdd(left, right) : b.CreateFAdd(left, right);
+    }
+#endif
+};
+
+struct NamePlus { static constexpr auto name = "add"; };
+using FunctionPlus = FunctionBinaryArithmetic<PlusImpl, NamePlus>;
+
+//void registerFunctionPlus(FunctionFactory & factory)
+//{
+//    factory.registerFunction<FunctionPlus>();
+//}
+void registerFunctionPlus(SimpleFunctionFactory& factory) {
+    factory.registerFunction<FunctionPlus>();
+}
+}
