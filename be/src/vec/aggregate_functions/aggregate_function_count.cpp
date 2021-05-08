7bb7d398b6634369c7b696ba031d61f18b0de47a
HappenLee <happenlee@hotmail.com>
2021-05-08 03:43:33 -0500

    1. Fix the core bug of agg query in DEBUG mode (#28)
    2. Support the count aggregate_function
    3. Some code refactor

--- be/src/vec/aggregate_functions/aggregate_function_count.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_count.cpp
@@ -15,27 +15,27 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// #include <AggregateFunctions/AggregateFunctionFactory.h>
+#include "vec/aggregate_functions/aggregate_function_simple_factory.h"
 #include <vec/aggregate_functions/aggregate_function_count.h>
 #include <vec/aggregate_functions/factory_helpers.h>
 
-namespace DB {
+namespace doris::vectorized {
 
-namespace {
+AggregateFunctionPtr createAggregateFunctionCount(const std::string & name, const DataTypes & argument_types, const Array & parameters) {
+    assertNoParameters(name, parameters);
+    assertArityAtMost<1>(name, argument_types);
 
-// AggregateFunctionPtr createAggregateFunctionCount(const std::string & name, const DataTypes & argument_types, const Array & parameters)
-// {
-//     assertNoParameters(name, parameters);
-//     assertArityAtMost<1>(name, argument_types);
-
-//     return std::make_shared<AggregateFunctionCount>(argument_types);
-// }
-
-} // namespace
+    return std::make_shared<AggregateFunctionCount>(argument_types);
+}
 
 // void registerAggregateFunctionCount(AggregateFunctionFactory & factory)
 // {
 //     factory.registerFunction("count", createAggregateFunctionCount, AggregateFunctionFactory::CaseInsensitive);
 // }
 
-} // namespace DB
+void registerAggregateFunctionCount(AggregateFunctionSimpleFactory& factory) {
+    factory.registerFunction("count", createAggregateFunctionCount);
+}
+
+} // namespace
+
