659b2d104260885c7259e090d62f68a117a639cf
stdpain <34912776+stdpain@users.noreply.github.com>
2021-05-11 15:32:15 +0800

    [1/2] support multi-distinct (#29)

---
+++ be/src/vec/aggregate_functions/aggregate_function_uniq.cpp
@@ -0,0 +1,48 @@
+#include <vec/aggregate_functions/aggregate_function_simple_factory.h>
+#include <vec/aggregate_functions/aggregate_function_uniq.h>
+#include <vec/aggregate_functions/factory_helpers.h>
+#include <vec/aggregate_functions/helpers.h>
+#include <vec/data_types/data_type_string.h>
+
+namespace doris::vectorized {
+
+namespace ErrorCodes {
+extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+} // namespace ErrorCodes
+
+template <template <typename> class Data, typename DataForVariadic>
+AggregateFunctionPtr createAggregateFunctionUniq(const std::string& name,
+                                                 const DataTypes& argument_types,
+                                                 const Array& params) {
+    assertNoParameters(name, params);
+
+    if (argument_types.empty())
+        throw Exception("Incorrect number of arguments for aggregate function " + name,
+                        ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    if (argument_types.size() == 1) {
+        const IDataType& argument_type = *argument_types[0];
+
+        AggregateFunctionPtr res(createWithNumericType<AggregateFunctionUniq, Data>(
+                *argument_types[0], argument_types));
+
+        WhichDataType which(argument_type);
+        // TODO: DateType
+        if (res)
+            return res;
+        else if (which.isStringOrFixedString())
+            return std::make_shared<AggregateFunctionUniq<String, Data<String>>>(argument_types);
+    }
+
+    return nullptr;
+}
+
+void registerAggregateFunctionsUniq(AggregateFunctionSimpleFactory& factory) {
+    AggregateFunctionCreator creator =
+            createAggregateFunctionUniq<AggregateFunctionUniqExactData,
+                                        AggregateFunctionUniqExactData<String>>;
+    factory.registerFunction("uniqExact", creator);
+}
+
+} // namespace doris::vectorized
