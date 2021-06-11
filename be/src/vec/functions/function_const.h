dc0d9bb2dd2babc20733be01f1371c336278f8e0
Zhengguo Yang <yangzhgg@gmail.com>
2021-06-11 19:36:24 +0800

    implement some math function sin asin cos acos tan atan ceil log log2 log10 sign sqrt floor (#66)

--- be/src/vec/functions/function_const.h
+++ be/src/vec/functions/function_const.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include "vec/columns/column_const.h"
 #include "vec/columns/columns_number.h"
 #include "vec/data_types/data_type_number.h"
 #include "vec/functions/function.h"
-#include "vec/columns/column_const.h"
 
 namespace doris::vectorized {
 
@@ -32,7 +32,7 @@ public:
 };
 
 template <typename Impl>
-class FunctionConst<Impl, false>: public IFunction {
+class FunctionConst<Impl, false> : public IFunction {
 public:
     static constexpr auto name = Impl::name;
     static FunctionPtr create() { return std::make_shared<FunctionConst>(); }
@@ -49,6 +49,29 @@ public:
         auto column = Impl::ReturnColVec::create();
         column->getData().emplace_back(Impl::init_value());
         block.getByPosition(result).column = ColumnConst::create(std::move(column), 1);
+        return Status::OK();
+    }
+};
+
+template <typename Impl>
+class FunctionMathConstFloat64 : public IFunction {
+public:
+    static constexpr auto name = Impl::name;
+    static FunctionPtr create() { return std::make_shared<FunctionMathConstFloat64>(); }
+
+private:
+    String getName() const override { return name; }
+
+    size_t getNumberOfArguments() const override { return 0; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes& /*arguments*/) const override {
+        return std::make_shared<DataTypeFloat64>();
+    }
+
+    Status executeImpl(Block& block, const ColumnNumbers&, size_t result,
+                       size_t input_rows_count) override {
+        block.getByPosition(result).column =
+                block.getByPosition(result).type->createColumnConst(input_rows_count, Impl::value);
         return Status::OK();
     }
 };
