051f78e270249bce2c3891eea9e3f063722e97aa
stdpain <drfeng08@gmail.com>
2021-07-05 22:47:41 +0800

    ---

--- be/src/olap/aggregate_func.h
+++ be/src/olap/aggregate_func.h
@@ -461,7 +461,7 @@ struct AggregateFuncTraits<OLAP_FIELD_AGGREGATION_HLL_UNION, OLAP_FIELD_TYPE_HLL
         // we use zero size represent this slice is a agg object
         dst_slice->size = 0;
         auto* hll = new HyperLogLog(*src_slice);
-        
+
         dst_slice->data = reinterpret_cast<char*>(hll);
 
         agg_pool->add(hll);
@@ -507,7 +507,7 @@ struct AggregateFuncTraits<OLAP_FIELD_AGGREGATION_BITMAP_UNION, OLAP_FIELD_TYPE_
         // we use zero size represent this slice is a agg object
         dst_slice->size = 0;
         auto bitmap = new BitmapValue(src_slice->data);
-        mem_pool->mem_tracker()->Consume(sizeof(BitmapValue));
+        //mem_pool->mem_tracker()->Consume(sizeof(BitmapValue));
         dst_slice->data = (char*)bitmap;
 
         agg_pool->add(bitmap);
