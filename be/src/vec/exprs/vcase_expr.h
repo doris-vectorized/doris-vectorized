762504f2de3ca6019250c060180aa3fb6e7a8422
wangbo <506340561@qq.com>
2021-07-09 19:31:41 +0800

    support case when
    refactor predicate function creator
    
    Revert "refactor predicate function creator"
    
    This reverts commit a824dc4f7e99cf6271d9f4e954feabc131079580.
    
    refactor predicate function creator
    
    Revert "refactor predicate function creator"
    
    This reverts commit 9fba7cd0b01be435772b55027796d9d90fb6219c.
    
    cow refactor
    
    Revert "cow refactor"
    
    This reverts commit 61f215aee00cc3c8cacc73f5c6df83589049d988.

---
+++ be/src/vec/exprs/vcase_expr.h
@@ -0,0 +1,51 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+#include "vec/exprs/vexpr.h"
+namespace doris::vectorized {
+ 
+class VCaseExpr : public VExpr {
+
+public:
+    VCaseExpr(const TExprNode& node);
+    ~VCaseExpr() = default;
+    virtual doris::Status execute(doris::vectorized::Block* block, int* result_column_id);
+    virtual doris::Status prepare(doris::RuntimeState* state, const doris::RowDescriptor& desc,
+                                  VExprContext* context);
+    virtual doris::Status open(doris::RuntimeState* state, VExprContext* context);
+    virtual void close(doris::RuntimeState* state, VExprContext* context);
+
+    virtual VCaseExpr* clone(doris::ObjectPool* pool) const override {
+        return pool->add(new VCaseExpr(*this));
+    }
+
+    virtual const std::string& expr_name() const override;
+
+ private:
+    doris::Status _execute_generic(doris::vectorized::Block* block, int* result_column_id);
+
+private:
+    bool _has_case_expr;
+    bool _has_else_expr;
+    // result data type
+    DataTypePtr _data_type;
+    const std::string _expr_name = "vcase expr";
+ 
+};
+
+} // namespace
