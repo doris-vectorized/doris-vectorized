d50c4161ceac9827c8d51e2032bfafc6cca38d38
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2021-09-17 12:47:25 +0800

    [Feature] add new node about mysql scan node (#169)
    Co-authored-by: BiteTheDDDDt <952130278@qq.com>

---
+++ be/src/vec/exec/vmysql_scan_node.cpp
@@ -0,0 +1,105 @@
+#include "vec/exec/vmysql_scan_node.h"
+
+#include "exec/text_converter.h"
+#include "exec/text_converter.hpp"
+#include "gen_cpp/PlanNodes_types.h"
+#include "runtime/row_batch.h"
+#include "runtime/runtime_state.h"
+#include "runtime/string_value.h"
+#include "runtime/tuple_row.h"
+#include "util/runtime_profile.h"
+#include "util/types.h"
+namespace doris::vectorized {
+
+VMysqlScanNode::VMysqlScanNode(ObjectPool* pool, const TPlanNode& tnode, const DescriptorTbl& descs)
+        : MysqlScanNode(pool, tnode, descs) {}
+
+VMysqlScanNode::~VMysqlScanNode() {}
+
+Status VMysqlScanNode::get_next(RuntimeState* state, vectorized::Block* block, bool* eos) {
+    VLOG_CRITICAL << "VMysqlScanNode::GetNext";
+    if (state == NULL || block == NULL || eos == NULL)
+        return Status::InternalError("input is NULL pointer");
+    if (!_is_init) return Status::InternalError("used before initialize.");
+    RETURN_IF_ERROR(exec_debug_action(TExecNodePhase::GETNEXT));
+    RETURN_IF_CANCELLED(state);
+    bool mem_reuse = block->mem_reuse();
+    DCHECK(block->rows() == 0);
+    std::vector<vectorized::MutableColumnPtr> columns(_slot_num);
+    bool mysql_eos = false;
+
+    do {
+        for (int i = 0; i < _slot_num; ++i) {
+            if (mem_reuse) {
+                columns[i] = std::move(*block->get_by_position(i).column).mutate();
+            } else {
+                columns[i] = _tuple_desc->slots()[i]->get_empty_mutable_column();
+            }
+        }
+        while (true) {
+            RETURN_IF_CANCELLED(state);
+            int batch_size = state->batch_size();
+            if (columns[0]->size() == batch_size) {
+                break;
+            }
+
+            char** data = NULL;
+            unsigned long* length = NULL;
+            RETURN_IF_ERROR(_mysql_scanner->get_next_row(&data, &length, &mysql_eos));
+
+            if (mysql_eos) {
+                *eos = true;
+                break;
+            }
+            int j = 0;
+            for (int i = 0; i < _slot_num; ++i) {
+                auto slot_desc = _tuple_desc->slots()[i];
+                if (!slot_desc->is_materialized()) {
+                    continue;
+                }
+
+                if (data[j] == nullptr) {
+                    if (slot_desc->is_nullable()) {
+                        auto* nullable_column =
+                                reinterpret_cast<vectorized::ColumnNullable*>(columns[i].get());
+                        nullable_column->insert_data(nullptr, 0);
+                    } else {
+                        std::stringstream ss;
+                        ss << "nonnull column contains NULL. table=" << _table_name
+                           << ", column=" << slot_desc->col_name();
+                        return Status::InternalError(ss.str());
+                    }
+                } else {
+                    RETURN_IF_ERROR(
+                            write_text_column(data[j], length[j], slot_desc, &columns[i], state));
+                }
+                j++;
+            }
+        }
+        auto n_columns = 0;
+        if (!mem_reuse) {
+            for (const auto slot_desc : _tuple_desc->slots()) {
+                block->insert(ColumnWithTypeAndName(std::move(columns[n_columns++]),
+                                                    slot_desc->get_data_type_ptr(),
+                                                    slot_desc->col_name()));
+            }
+        } else {
+            columns.clear();
+        }
+        VLOG_ROW << "VMYSQLScanNode output rows: " << block->rows();
+    } while (block->rows() == 0 && !(*eos));
+    return Status::OK();
+}
+
+Status VMysqlScanNode::write_text_column(char* value, int value_length, SlotDescriptor* slot,
+                                         vectorized::MutableColumnPtr* column_ptr,
+                                         RuntimeState* state) {
+    if (!_text_converter->write_column(slot, column_ptr, value, value_length, true, false)) {
+        std::stringstream ss;
+        ss << "Fail to convert mysql value:'" << value << "' to " << slot->type() << " on column:`"
+           << slot->col_name() + "`";
+        return Status::InternalError(ss.str());
+    }
+    return Status::OK();
+}
+} // namespace doris::vectorized
