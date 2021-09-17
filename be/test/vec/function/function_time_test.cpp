f06cade7dc297dd88b47db1d002e88a298124a30
BiteTheDDDDt <952130278@qq.com>
2021-09-17 20:41:52 +0800

    promote code

--- be/test/vec/function/function_time_test.cpp
+++ be/test/vec/function/function_time_test.cpp
@@ -31,16 +31,17 @@
 namespace doris {
 
 using vectorized::Null;
+using vectorized::DataSet;
+using vectorized::TypeIndex;
 
 TEST(TimestampFunctionsTest, day_of_week_test) {
     std::string func_name = "dayofweek";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("2001-02-03 12:34:56")}, 7},
-            {{std::string("2020-00-01 00:00:00")}, Null()},
-            {{std::string("2020-01-00 00:00:00")}, Null()}};
+    DataSet data_set = {{{std::string("2001-02-03 12:34:56")}, 7},
+                        {{std::string("2020-00-01 00:00:00")}, Null()},
+                        {{std::string("2020-01-00 00:00:00")}, Null()}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -48,12 +49,11 @@ TEST(TimestampFunctionsTest, day_of_week_test) {
 TEST(TimestampFunctionsTest, day_of_month_test) {
     std::string func_name = "dayofmonth";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("2020-00-01 00:00:00")}, Null()},
-            {{std::string("2020-01-01 00:00:00")}, 1},
-            {{std::string("2020-02-29 00:00:00")}, 29}};
+    DataSet data_set = {{{std::string("2020-00-01 00:00:00")}, Null()},
+                        {{std::string("2020-01-01 00:00:00")}, 1},
+                        {{std::string("2020-02-29 00:00:00")}, 29}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -61,12 +61,11 @@ TEST(TimestampFunctionsTest, day_of_month_test) {
 TEST(TimestampFunctionsTest, day_of_year_test) {
     std::string func_name = "dayofyear";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("2020-00-01 00:00:00")}, Null()},
-            {{std::string("2020-01-00 00:00:00")}, Null()},
-            {{std::string("2020-02-29 00:00:00")}, 60}};
+    DataSet data_set = {{{std::string("2020-00-01 00:00:00")}, Null()},
+                        {{std::string("2020-01-00 00:00:00")}, Null()},
+                        {{std::string("2020-02-29 00:00:00")}, 60}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -74,12 +73,11 @@ TEST(TimestampFunctionsTest, day_of_year_test) {
 TEST(TimestampFunctionsTest, week_of_year_test) {
     std::string func_name = "weekofyear";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("2020-00-01 00:00:00")}, Null()},
-            {{std::string("2020-01-00 00:00:00")}, Null()},
-            {{std::string("2020-02-29 00:00:00")}, 9}};
+    DataSet data_set = {{{std::string("2020-00-01 00:00:00")}, Null()},
+                        {{std::string("2020-01-00 00:00:00")}, Null()},
+                        {{std::string("2020-02-29 00:00:00")}, 9}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -87,12 +85,11 @@ TEST(TimestampFunctionsTest, week_of_year_test) {
 TEST(TimestampFunctionsTest, year_test) {
     std::string func_name = "year";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("2021-01-01 00:00:00")}, 2021},
-            {{std::string("2021-01-00 00:00:00")}, Null()},
-            {{std::string("2025-05-01 00:00:00")}, 2025}};
+    DataSet data_set = {{{std::string("2021-01-01 00:00:00")}, 2021},
+                        {{std::string("2021-01-00 00:00:00")}, Null()},
+                        {{std::string("2025-05-01 00:00:00")}, 2025}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -100,10 +97,10 @@ TEST(TimestampFunctionsTest, year_test) {
 TEST(TimestampFunctionsTest, unix_timestamp_test) {
     std::string func_name = "unix_timestamp";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{std::string("9999-12-30 00:00:00")}, 0}, {{std::string("1000-01-01 00:00:00")}, 0}};
+    DataSet data_set = {{{std::string("9999-12-30 00:00:00")}, 0},
+                        {{std::string("1000-01-01 00:00:00")}, 0}};
 
     vectorized::check_function<vectorized::DataTypeInt32, true>(func_name, input_types, data_set);
 }
@@ -111,10 +108,9 @@ TEST(TimestampFunctionsTest, unix_timestamp_test) {
 TEST(TimestampFunctionsTest, from_unix_test) {
     std::string func_name = "from_unixtime";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::Int32};
+    std::vector<std::any> input_types = {TypeIndex::Int32};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-            {{1565080737}, std::string("2019-08-06 16:38:57")}, {{-123}, Null()}};
+    DataSet data_set = {{{1565080737}, std::string("2019-08-06 16:38:57")}, {{-123}, Null()}};
 
     vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types, data_set);
 }
@@ -122,10 +118,9 @@ TEST(TimestampFunctionsTest, from_unix_test) {
 TEST(TimestampFunctionsTest, timediff_test) {
     std::string func_name = "timediff";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime,
-                                         vectorized::TypeIndex::DateTime};
+    std::vector<std::any> input_types = {TypeIndex::DateTime, TypeIndex::DateTime};
 
-    std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
+    DataSet data_set = {
             {{std::string("2019-07-18 12:00:00"), std::string("2019-07-18 12:00:00")}, 0.0},
             {{std::string("2019-07-18 12:00:00"), std::string("2019-07-18 13:01:02")}, -3662.0},
             {{std::string("2019-00-18 12:00:00"), std::string("2019-07-18 13:01:02")}, Null()},
@@ -137,26 +132,24 @@ TEST(TimestampFunctionsTest, timediff_test) {
 TEST(TimestampFunctionsTest, date_format_test) {
     std::string func_name = "date_format";
 
-    std::vector<std::any> input_types = {vectorized::TypeIndex::DateTime,
-                                         vectorized::Consted {vectorized::TypeIndex::String}};
+    std::vector<std::any> input_types = {TypeIndex::DateTime,
+                                         vectorized::Consted {TypeIndex::String}};
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-                {{std::string("2009-10-04 22:23:00"), std::string("%W %M %Y")},
-                 std::string("Sunday October 2009")}};
+        DataSet data_set = {{{std::string("2009-10-04 22:23:00"), std::string("%W %M %Y")},
+                             std::string("Sunday October 2009")}};
 
         vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types,
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-                {{std::string("2007-10-04 22:23:00"), std::string("%H:%i:%s")},
-                 std::string("22:23:00")}};
+        DataSet data_set = {{{std::string("2007-10-04 22:23:00"), std::string("%H:%i:%s")},
+                             std::string("22:23:00")}};
 
         vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types,
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
+        DataSet data_set = {
                 {{std::string("1900-10-04 22:23:00"), std::string("%D %y %a %d %m %b %j")},
                  std::string("4th 00 Thu 04 10 Oct 277")}};
 
@@ -164,7 +157,7 @@ TEST(TimestampFunctionsTest, date_format_test) {
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
+        DataSet data_set = {
                 {{std::string("1997-10-04 22:23:00"), std::string("%H %k %I %r %T %S %w")},
                  std::string("22 22 10 10:23:00 PM 22:23:00 00 6")}};
 
@@ -172,22 +165,21 @@ TEST(TimestampFunctionsTest, date_format_test) {
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
-                {{std::string("1999-01-01 00:00:00"), std::string("%X %V")},
-                 std::string("1998 52")}};
+        DataSet data_set = {{{std::string("1999-01-01 00:00:00"), std::string("%X %V")},
+                             std::string("1998 52")}};
 
         vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types,
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
+        DataSet data_set = {
                 {{std::string("2006-06-01 00:00:00"), std::string("%d")}, std::string("01")}};
 
         vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types,
                                                                      data_set);
     }
     {
-        std::vector<std::pair<std::vector<std::any>, std::any>> data_set = {
+        DataSet data_set = {
                 {{std::string("2006-06-01 00:00:00"), std::string("%%%d")}, std::string("%01")}};
 
         vectorized::check_function<vectorized::DataTypeString, true>(func_name, input_types,
