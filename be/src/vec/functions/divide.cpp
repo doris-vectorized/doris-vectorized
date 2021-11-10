7996f5b1e08336ae5d6a48a8e04f8ce2cbdfbc8e
Zeno Yang <cookie.yz@qq.com>
2021-11-10 15:49:31 +0800

    [Function] Support divide and mod functions to return NULL when divided by 0 (#223)

--- be/src/vec/functions/divide.cpp
+++ be/src/vec/functions/divide.cpp
@@ -15,7 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "vec/functions/function_binary_arithmetic.h"
+
+#include "vec/functions/function_binary_arithmetic_to_null_type.h"
 #include "vec/functions/simple_function_factory.h"
 
 namespace doris::vectorized {
@@ -26,15 +27,16 @@ struct DivideFloatingImpl {
     static const constexpr bool allow_decimal = true;
 
     template <typename Result = ResultType>
-    static inline Result apply(A a, B b) {
-        return static_cast<Result>(a) / b;
+    static inline Result apply(A a, B b, NullMap& null_map, size_t index) {
+        null_map[index] = b == 0;
+        return static_cast<Result>(a) / (b + (b == 0));
     }
 };
 
 struct NameDivide {
     static constexpr auto name = "divide";
 };
-using FunctionDivide = FunctionBinaryArithmetic<DivideFloatingImpl, NameDivide>;
+using FunctionDivide = FunctionBinaryArithmeticToNullType<DivideFloatingImpl, NameDivide>;
 
 void register_function_divide(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionDivide>();
