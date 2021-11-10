11d53c2a1c7025440046cacf104188cb330c74d0
Zeno Yang <cookie.yz@qq.com>
2021-11-10 15:49:31 +0800

    [Function] Support divide and mod functions to return NULL when divided by 0 (#223)

--- be/src/vec/functions/int_div.h
+++ be/src/vec/functions/int_div.h
@@ -18,8 +18,11 @@
 #pragma once
 
 #include "common/compiler_util.h"
+#include "common/logging.h"
 #include "common/status.h"
+#include "runtime/decimalv2_value.h"
 #include "type_traits"
+#include "vec/columns/column_nullable.h"
 #include "vec/common/exception.h"
 #include "vec/data_types/number_traits.h"
 
@@ -27,35 +30,6 @@ namespace doris::vectorized {
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsign-compare"
-
-template <typename A, typename B>
-inline void throw_if_division_leads_to_fpe(A a, B b) {
-    /// Is it better to use siglongjmp instead of checks?
-
-    // TODO: Support return NULL in the future
-    if (UNLIKELY(b == 0)) {
-//        throw Exception("Division by zero", TStatusCode::VEC_ILLEGAL_DIVISION);
-    }
-
-    /// http://avva.livejournal.com/2548306.html
-    if (UNLIKELY(std::is_signed_v<A> && std::is_signed_v<B> && a == std::numeric_limits<A>::min() &&
-                 b == -1)) {
-        throw Exception("Division of minimal signed number by minus one",
-                        TStatusCode::VEC_ILLEGAL_DIVISION);
-    }
-}
-
-template <typename A, typename B>
-inline bool division_leads_to_fpe(A a, B b) {
-    if (UNLIKELY(b == 0)) return true;
-
-    if (UNLIKELY(std::is_signed_v<A> && std::is_signed_v<B> && a == std::numeric_limits<A>::min() &&
-                 b == -1))
-        return true;
-
-    return false;
-}
-
 #pragma GCC diagnostic pop
 
 template <typename A, typename B>
@@ -63,17 +37,16 @@ struct DivideIntegralImpl {
     using ResultType = typename NumberTraits::ResultOfIntegerDivision<A, B>::Type;
 
     template <typename Result = ResultType>
-    static inline Result apply(A a, B b) {
-        throw_if_division_leads_to_fpe(a, b);
+    static inline Result apply(A a, B b, NullMap& null_map, size_t index) {
+        null_map[index] = b == 0;
 
-        if (UNLIKELY(b == 0)) return 0;
         /// Otherwise overflow may occur due to integer promotion. Example: int8_t(-1) / uint64_t(2).
         /// NOTE: overflow is still possible when dividing large signed number to large unsigned number or vice-versa. But it's less harmful.
         if constexpr (std::is_integral_v<A> && std::is_integral_v<B> &&
                       (std::is_signed_v<A> || std::is_signed_v<B>))
-            return std::make_signed_t<A>(a) / std::make_signed_t<B>(b);
+            return std::make_signed_t<A>(a) / (std::make_signed_t<B>(b) + (b == 0));
         else
-            return a / b;
+            return a / (b + (b == 0));
     }
 };
 
