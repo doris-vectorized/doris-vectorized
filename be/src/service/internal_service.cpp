bb2c70977cf7bb620769ecd8004b4716a2f1985a
lihaopeng <lihaopeng@baidu.com>
2021-12-10 15:46:15 +0800

    [Fold Constant] Support vec fold constant

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -419,7 +419,10 @@ Status PInternalServiceImpl<T>::_fold_constant_expr(const std::string& ser_reque
         uint32_t len = ser_request.size();
         RETURN_IF_ERROR(deserialize_thrift_msg(buf, &len, false, &t_request));
     }
-    return FoldConstantExecutor().fold_constant_expr(t_request, response);
+    if (!t_request.__isset.vec_exec || !t_request.vec_exec)
+        return FoldConstantExecutor().fold_constant_expr(t_request, response);
+
+    return FoldConstantExecutor().fold_constant_vexpr(t_request, response);
 }
 
 template <typename T>
