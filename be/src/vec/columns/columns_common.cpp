9bf9d675e49f6cbcbcc870b81216f6d98a8c0351
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2021-12-15 13:59:02 +0800

    [DOC] Add a short of license (#282)

--- be/src/vec/columns/columns_common.cpp
+++ be/src/vec/columns/columns_common.cpp
@@ -14,6 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+// This file is copied from
+// https://github.com/ClickHouse/ClickHouse/blob/master/src/Columns/ColumnsCommon.cpp
+// and modified by Doris
 
 #ifdef __SSE2__
 #include <emmintrin.h>
--- be/src/vec/functions/function_bitmap.cpp
+++ be/src/vec/functions/function_bitmap.cpp
@@ -14,6 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+// This file is copied from
+// https://github.com/ClickHouse/ClickHouse/blob/master/src/Functions/FunctionBitmap.h
+// and modified by Doris
 
 #include "util/string_parser.hpp"
 #include "vec/functions/function_totype.h"
